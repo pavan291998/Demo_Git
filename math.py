8684019cc4a412d7a1b379c16f5b768a1f7f4895
Pavan Patil <pavanpatil291998@gmail.com>
2021-01-25 14:59:39 +0530

    Added Math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+# Perform addition on x and y
+def add(x,y):
+    return x+y;
+# Perform subtraction
+def subtract(x,y):
+	if(x<y)return error;
+    return x-y;
+# Perform multiplication
+def multiply(x,y):
+    return xe*ye;
+    
+# Perform division
+def divide(x,y):
+    return x/y;
+
+def square(x)
+	return x*x
